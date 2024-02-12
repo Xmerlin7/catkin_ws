00ec1b11bbc6c0e52a6581eb668ce3905a133d22
Seif Allah <saifeldarageely@gmail.com>
2024-02-12 19:26:33 +0200

    #

---
+++ build/MRTbot/mrt_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/seif/catkin_ws/src/MRTbot/mrt_description
+# Build directory: /home/seif/catkin_ws/build/MRTbot/mrt_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/MRTbot/spcbot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/seif/catkin_ws/src/MRTbot/spcbot_description
+# Build directory: /home/seif/catkin_ws/build/MRTbot/spcbot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
